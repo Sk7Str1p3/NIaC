69a62c15996b33ed180df1004a7383da79758f6c
Sk7Str1p3 <finenkofedor@gmail.com>
2025-09-20 19:23:05 +0000

    chore: flake description

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
-  description = "A very basic flake";
+  description = "Sk7Str1p3's NixOS Infrastructure-as-a-Code (NIaC (Nyac <3))";
 
+  # The `flake.lock` generation is not computable[1].
+  # So far it's impossible to
+  # add helper functions to manage inputs.
+  # ... But maybe we can work around this issue with git hooks
+  # and shell scripts?
+  #
+  # [1]: https://github.com/NixOS/nix/issues/5373
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -14,13 +21,13 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-    ];
+      systems = [
+        "x86_64-linux"
+      ];
 
-    imports = [
+      imports = [
         ./packages/flakeMod.nix
-      ./shells/self.nix
-    ];
-  };
+        ./shells/self.nix
+      ];
+    };
 }
