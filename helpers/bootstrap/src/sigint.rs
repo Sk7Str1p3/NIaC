61c3849a8dfc76ed83f998b003eed93be29ab4e4
Sk7Str1p3 <finenkofedor@gmail.com>
2025-11-06 05:19:46 +0000

    chore(bootstrap::sigint): update documentation

--- helpers/bootstrap/src/sigint.rs
+++ helpers/bootstrap/src/sigint.rs
@@ -1,15 +1,34 @@
-//! ## Ctrl+C (SIGINT)
-//! Functions executed if job was interrupted with SIGINT
-
+//! ## SIGINT
+//! This module provides functionality to gracefully handle
+//! program interruption by cleaning up temporary
+//! directories when the user presses Ctrl+C.
 use std::sync::Mutex;
 
 use color_eyre::Result;
 use color_eyre::eyre::Context as _;
 
-/// Static value indicates path to $OUT.
-/// This path dropped on SIGINT.
+/// Holds the path to a temporary directory that needs to be
+/// cleaned up when the program is interrupted.
+///
+/// The path stored in this variable is automatically
+/// removed when a SIGINT signal (Ctrl+C) is received by the
+/// program. This ensures proper cleanup of temporary
+/// resources even during unexpected program termination.
+///
+/// ### Example
+/// ```
+/// use crate::sigint::TMPDIR;
+/// let path = "/tmp/my_temp_dir";
+/// // ...
+/// tracing::info!("TMPDIR created at: {}", path)
+/// // Store a temporary directory path in global state
+/// *TMPDIR.lock().unwrap() = String::from(path);
+/// ```
 pub static TMPDIR: Mutex<String> = Mutex::new(String::new());
 
+/// Initializes the SIGINT (Ctrl+C) handler to gracefully
+/// exit and clean up the temporary directory stored in
+/// [`TMPDIR`].
 #[inline]
 pub fn init() -> Result<()> {
     ctrlc::set_handler(|| {
